28ae1bca6be379b70867663d8f284fdf2f315145
thesonofwind99 <dunhan999@gmail.com>
2024-11-25 23:25:06 +0700

    #35 show fullname of staff in header

--- src/layouts/staff/component/Header.tsx
+++ src/layouts/staff/component/Header.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../assets/css/styles.css'
-
+import { AuthContext } from "../../customer/component/AuthContext";
 const Header: React.FC<{ toggleId: string }> = ({ toggleId }) => {
+  const {employeeFullName} = useContext(AuthContext);
   return (
     <header className="header" id="header">
       <div className="header__container">
         <a href="#" className="header__logo">
           <i className="ri-cloud-fill"></i>
-          <span>Nguyễn Quang Hoài Nam</span>
+          {employeeFullName ? (<span>{employeeFullName}</span>): (<span>None employee</span>)}        
         </a>
         <button className="header__toggle" id={toggleId}>
           <i className="ri-menu-line"></i>
