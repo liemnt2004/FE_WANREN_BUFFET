d0e88c4e72182e5f9ae5256675bf4c4d6e9b0966
MyXen <tntthliem@gmail.com>
2024-12-05 11:30:19 +0700

    deloy

--- src/layouts/customer/indexCustomer.tsx
+++ src/layouts/customer/indexCustomer.tsx
@@ -310,84 +310,38 @@ const IndexCustomer: React.FC = () => {
                                 <div className="row">
                                     <div className="col-md-6 help" style={{paddingLeft: 0, paddingRight: '50px'}}>
                                         <h4 className="fw-bold pb-4">CHÚNG TÔI CÓ THỂ GIÚP GÌ CHO BẠN ?</h4>
-                                        <div className="accordion" id="accordionExample">
-                                            {/* Accordion Item 1 */}
-                                            <div className="accordion-item my-3 mt-0 rounded-0">
-                                                <h2 className="accordion-header rounded-0">
-                                                    <button
-                                                        className="accordion-button accordion-item-design collapsed rounded-0"
-                                                        type="button" data-bs-toggle="collapse"
-                                                        data-bs-target="#collapseOne" aria-expanded="false"
-                                                        aria-controls="collapseOne">
-                                                        Accordion Item #1
-                                                    </button>
-                                                </h2>
-                                                <div id="collapseOne" className="accordion-collapse collapse"
-                                                     data-bs-parent="#accordionExample">
-                                                    <div className="accordion-body">
-                                                        <strong>This is the first item's accordion body.</strong> It is
-                                                        hidden by default.
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            {/* Accordion Item 2 */}
-                                            <div className="accordion-item my-3 rounded-0">
-                                                <h2 className="accordion-header rounded-0">
-                                                    <button
-                                                        className="accordion-button accordion-item-design collapsed rounded-0"
-                                                        type="button" data-bs-toggle="collapse"
-                                                        data-bs-target="#collapseTwo" aria-expanded="false"
-                                                        aria-controls="collapseTwo">
-                                                        Accordion Item #2
-                                                    </button>
-                                                </h2>
-                                                <div id="collapseTwo" className="accordion-collapse collapse"
-                                                     data-bs-parent="#accordionExample">
-                                                    <div className="accordion-body">
-                                                        <strong>This is the second item's accordion body.</strong> It is
-                                                        hidden by default.
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            {/* Accordion Item 3 */}
-                                            <div className="accordion-item my-3 rounded-0">
-                                                <h2 className="accordion-header rounded-0">
-                                                    <button
-                                                        className="accordion-button accordion-item-design collapsed rounded-0"
-                                                        type="button" data-bs-toggle="collapse"
-                                                        data-bs-target="#collapseThree" aria-expanded="false"
-                                                        aria-controls="collapseThree">
-                                                        Accordion Item #3
-                                                    </button>
-                                                </h2>
-                                                <div id="collapseThree" className="accordion-collapse collapse"
-                                                     data-bs-parent="#accordionExample">
-                                                    <div className="accordion-body">
-                                                        <strong>This is the third item's accordion body.</strong> It is
-                                                        hidden by default.
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            {/* Accordion Item 4 */}
-                                            <div className="accordion-item my-3 rounded-0">
-                                                <h2 className="accordion-header rounded-0">
-                                                    <button
-                                                        className="accordion-button accordion-item-design collapsed rounded-0"
-                                                        type="button" data-bs-toggle="collapse"
-                                                        data-bs-target="#collapseFour" aria-expanded="false"
-                                                        aria-controls="collapseFour">
-                                                        Accordion Item #4
-                                                    </button>
-                                                </h2>
-                                                <div id="collapseFour" className="accordion-collapse collapse"
-                                                     data-bs-parent="#accordionExample">
-                                                    <div className="accordion-body">
-                                                        <strong>This is the fourth item's accordion body.</strong> It is
-                                                        hidden by default.
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <div className="accordion accordion-flush" id="accordionFlushExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="flush-headingOne">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="flush-headingTwo">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header" id="flush-headingThree">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+    </div>
+  </div>
+</div>
                                     </div>
                                     <div className="col-md-6" style={{paddingRight: 0}}>
                                         <h4 className="fw-bold pb-4">KHÁCH HÀNG NÓI GÌ?</h4>
